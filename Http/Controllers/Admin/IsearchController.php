a296ff196696749015d7ca3742198efdc3b7428f
Marcos Rativa <marcosr@imaginacolombia.com>
2018-10-18 12:00:19 -0500

    change Controller Admin

--- Http/Controllers/Admin/IsearchController.php
+++ Http/Controllers/Admin/IsearchController.php
@@ -4,9 +4,9 @@ namespace Modules\Isearch\Http\Controllers\Admin;
 
 use Modules\Core\Http\Controllers\Admin\AdminBaseController;
 use Modules\Workshop\Manager\ModuleManager;
-use MongoDB\Driver\Exception\AuthenticationException;
 use Nwidart\Modules\Module;
 use Nwidart\Modules\Repository;
+
 class IsearchController extends AdminBaseController
 {
 
@@ -24,38 +24,10 @@ class IsearchController extends AdminBaseController
 
     private $auth;
 
-    public function __construct(Authentication $auth, ModuleManager $moduleManager, Repository $modules)
+    public function __construct()
     {
         parent::__construct();
-        $this->auth = $auth;
-        $this->moduleManager = $moduleManager;
-        $this->modules = $modules;
-        $driver = config('asgard.user.config.driver');
+
     }
-
-    /**
-     * Display a list of all modules
-     * @return View
-     */
-    public function index()
-    {
-        $modules = $this->modules->all();
-
-        return view('isearch::admin.index', compact('modules'));
-    }
-    /*   public function update( UpdateSearchRequest $request)
-      {
-      $this->modules->update($page, $request->all());
-
-          if ($request->get('button') === 'index') {
-              return redirect()->route('admin.page.page.index')
-                  ->withSuccess(trans('page::messages.page updated'));
-          }
-
-          return redirect()->back()
-              ->withSuccess(trans('page::messages.page updated'));
-      }
-
-}*/
 
 }
