2d78c014968faade42aca19ab5d283119b25333f
Carlos <carlosdevia@imaginacolombia.com>
2020-11-30 12:13:03 -0500

    Fix: Module Isearch don't have admin routes right now.

--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -4,10 +4,10 @@ use Illuminate\Routing\Router;
 /** @var Router $router */
 
 $router->group(['prefix' =>'/isearch'], function (Router $router) {
-    $router->get('modules', [
+    /*$router->get('isearch', [
         'as' => 'admin.isearch.modules.index',
         'uses' => 'IsearchController@index',
         //'middleware' => 'can:page.pages.index',
-    ]);
+    ]);*/
 });
 
