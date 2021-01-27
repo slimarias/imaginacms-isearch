8d04313ba6287602e2a25a1d71f5e9f0c16fbbf8
Slim Arias Sanchez <slimarias1987@gmail.com>
2021-01-27 10:27:37 -0500

    +FIX: search modal logo

--- Resources/views/frontend/livewire/search/layouts/search-layout-1/index.blade.php
+++ Resources/views/frontend/livewire/search/layouts/search-layout-1/index.blade.php
@@ -8,7 +8,7 @@
             <div class="modal-content">
                 <div class="modal-body">
                     <div class="text-center">
-                        <img src="{{ Theme::url('img/logo.png') }}" class="img-fluid mx-auto py-2"/>
+                        <x-isite::logo />
                     </div>
                     <h5 class="text-center my-4 font-weight-bold">
                         {{ $title }}
